26d7f52fa9150b7863a9b63bb17b72e73b39dedd
Ayush Gupta <ayush90.delhi@gmail.com>
2016-02-26 00:16:27 -0500

    Plugin Done

--- plugin.py
+++ plugin.py
@@ -1,5 +1,12 @@
 import sublime, sublime_plugin
+import webbrowser
 
-class ExampleCommand(sublime_plugin.TextCommand):
+class PyrecoCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
-		self.view.insert(edit, 0, "Hello, World!")
+		sel = self.view.sel()
+		url = "http://127.0.0.1/?tag="
+		for region in sel:
+			sel_text = self.view.substr(region)
+			url += sel_text + ";"
+		print(url)
+		webbrowser.open_new_tab(url)
