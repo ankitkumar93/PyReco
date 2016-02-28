d88f7eff3ed6656d81ae966e22d37dfb0d037c29
hkaur <harsh.gaggu@gmail.com>
2016-02-28 18:43:37 -0500

    Added code for filtering tagged words

--- Bagofwords.py
+++ Bagofwords.py
@@ -24,61 +24,33 @@ question_cursor.batch_size(50000)
 
 # load nltk's English stopwords as variable called 'stopwords'
 stopwords = nltk.corpus.stopwords.words('english')
-stopFile = open("stopwords.txt", "r")
+stopFile = open("/home/harshdeep/Documents/SE/stopwords.txt", "r")
 stopW = stopFile.read()
 my_stopwords = stopW.split(", ")
-updatedstopwords = my_stopwords + stopwords
-
+updatedstopwords = my_stopwords + stopwords + ['python']
+print(len(updatedstopwords))
 
 # load nltk's SnowballStemmer as variabled 'stemmer'
 from nltk.stem.snowball import SnowballStemmer
 stemmer = SnowballStemmer("english")
 
 def main():
-	questions = []
 	Id = []
-	title = []
-	tags = []
 	for question in question_cursor:
-		questions.append(question["Body"])
 		Id.append(question["Id"])
-		title.append(question["Title"])
-		tags.append(question["Tags"])
+		Create_BagOfWords(question["Body"] + question["Title"] + question["Tags"])
 
-	print(questions[2])
-	sys.exit(1)
+			
 	
+def Create_BagOfWords(text):
+	text = re.sub(r'python|Python|[^A-Za-z0-9. ]+',' ',text)
+	tokens = nltk.word_tokenize(text)
+	tokens = set(tokens)
+	tagged = nltk.pos_tag(tokens)
+	tagged = [word for (word,tag) in tagged if tag not in ["PRP","RB","JJ", "JJS"] and word.lower() not in updatedstopwords]
+	text = ' '.join([word for word in tagged])
 
 
-def text_preprocess(words):
-	res = ""
-	words = words.split()
-	for word in words:
-		if word.lower() not in my_stopwords:
-			res = res + " " + word
-	return res
-
-
-def tokenize_and_stem(text):
-	# first tokenize by sentence, then by word to ensure that punctuation is caught as it's own token
-	tokens = [word for sent in nltk.sent_tokenize(text) for word in nltk.word_tokenize(sent)]
-	filtered_tokens = []
-	# filter out any tokens not containing letters (e.g., numeric tokens, raw punctuation)
-	for token in tokens:
-		if re.search('[a-zA-Z]', token):
-			filtered_tokens.append(token)
-	stems = [stemmer.stem(t) for t in filtered_tokens]
-	return stems
-
-def tokenize_only(text):
-    # first tokenize by sentence, then by word to ensure that punctuation is caught as it's own token
-    tokens = [word.lower() for sent in nltk.sent_tokenize(text) for word in nltk.word_tokenize(sent)]
-    filtered_tokens = []
-    # filter out any tokens not containing letters (e.g., numeric tokens, raw punctuation)
-    for token in tokens:
-        if re.search('[a-zA-Z]', token):
-            filtered_tokens.append(token)
-    return filtered_tokens
 
 if __name__ == "__main__":
     main()
