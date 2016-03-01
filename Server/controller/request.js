bbf490f6d870249e3ff2515be853968dadd03440
akumar18 <akumar18@ncsu.edu>
2016-03-01 15:26:54 -0500

    Solution 1 Ready

---
+++ Server/controller/request.js
@@ -0,0 +1,58 @@
+//Require
+var request = require('request');
+var mongoose = require("mongoose");
+
+//Db
+var db = 'pyreco';
+
+//Helper Functions
+function connecttodb(){
+	mongoose.connect('mongodb://localhost/'+db);
+}
+
+function closeconnectiontodb(){
+	mongoose.connection.close();
+}
+
+//Helper Functions
+function getQuestions(ids, res){
+	request.post(
+		'http://127.0.0.1:8080/ques',
+		{form: {ids: ids}},
+		function (err, response, body){
+			if(!err && response.statusCode == 200){
+				res.send(body);
+			}else{
+				res.send("questions: " + err);
+			}
+			closeconnectiontodb();
+		}
+	);
+}
+
+function getRecommendation(tags, res){
+	request.post(
+		'http://127.0.0.1:8080/recom',
+		{form: {tags: tags}},
+		function (err, response, body){
+			if(!err && response.statusCode == 200){
+				var bodyJSON = JSON.parse(body);
+				getQuestions(bodyJSON.ids, res);
+			}else{
+				res.send("recommendor: " + err);
+				closeconnectiontodb();
+			}
+		}
+	);
+}
+
+//Class
+var requestManager = {
+	handleRequest: function(req, res){
+		var query = req.body.tag;
+		var tags = query.split(';');
+		connecttodb();
+		getRecommendation(tags, res);
+	}
+};
+module.exports = requestManager;
---
+++ Server/server.js
@@ -0,0 +1,36 @@
+//Import
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+
+//Controllers
+var controller = require("./controller/request.js");
+var recommender = require('./controller/recommender.js');
+var question = require('./controller/question.js');
+var answer = require('./controller/answer.js');
+
+//Init
+var app = express();
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
+//Server Start
+app.listen(8080, function(){
+	console.log("Server listening now!");
+});
+//Post Requests
+app.post('/req', function(req,res){
+	controller.handleRequest(req, res);
+});
+
+app.post('/recom', function(req, res){
+	recommender.handleRecomm(req, res);
+});
+
+app.post('/ques', function(req, res){
+	question.handleQuestions(req, res);
+});
