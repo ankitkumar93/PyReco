662d8268d8702049c0ec866af2a1d484a50cdef2
akumar18 <akumar18@ncsu.edu>
2016-03-29 04:09:50 -0400

    Simhash filter in progress

--- Server/controller/hashfilter.js
+++ Server/controller/hashfilter.js
@@ -1,11 +1,23 @@
-var simhash = require('simhash')('md5');
+var simhash = require('simhash')();
+
+function getHashVal(hasharr){
+	var hashval = 0;
+	var length = hasharr.length;
+	for(index in hasharr){
+		if(hasharr[length - index - 1] == 1)
+			hashval += Math.pow(2, index);
+	}
+
+	return hashval;
+}
 
 var hashfilter = {
 	filter: function(body, context){
 		var ids = JSON.parse(body).ids;
 		//var context_tags = context.split(" ");
 
-		var hashval = simhash(context);
+		var hashdata = simhash(context);
+		var hashval = getHashVal(hashdata);
 		console.log(hashval);
 
 		return ids;
