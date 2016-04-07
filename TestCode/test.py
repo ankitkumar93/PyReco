a1b237fd1c6c77c1f3a58b5fb00d40056aac1cf5
akumar18 <akumar18@ncsu.edu>
2016-04-07 18:45:26 -0400

    Done

--- TestCode/test.py
+++ TestCode/test.py
@@ -1,4 +1,14 @@
-l1 = {"a"}
-l2 = {"b"}
 #create a dictionary out of 2 lists: l1 and l2. l1 is a set of keys and l2 is the set of corresponding values
-out = dict(zip(l1,l2))
+out = dict(l1,l2)
+
+#check if a key 'k' exists in a dictionary 'd'
+if d[key] == null
+
+#find the time now
+time = datetime.time()
+
+#split a sentence 'sent' into words and store it in an array 'arr'
+arr = split(sent)
+
+#you have a list of words 'words' and need to sort it and store it in sorted_words
+sorted_words = sort(words)
