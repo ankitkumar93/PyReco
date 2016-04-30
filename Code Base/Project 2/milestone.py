38907cf7acd185f69e66a8626dbbeee51929ee58
hkaur <harsh.gaggu@gmail.com>
2016-04-30 16:00:07 -0400

    updating column name changes

--- Code Base/Project 2/milestone.py
+++ Code Base/Project 2/milestone.py
@@ -1,12 +1,13 @@
 #Created by Ankit Kumar(ankitkumar93) for CSC-510 Project 2 Sprint 2016
 from __future__ import print_function
 from pymongo import MongoClient
+import sys
 
 #db
 MONGO_CONNECTION_STRING = "mongodb://localhost:27017/"
 #names
-dbname = "se_project2"
-coll_name_milestone_raw = "milestone_raw"
+dbname = "project2"
+coll_name_milestone_raw = "raw_milestones"
 coll_name_milestones = "milestones"
 coll_name_issues = "issues"
 
@@ -16,30 +17,6 @@ coll_name_milestone_raw = db[coll_name_milestone_raw]
 coll_name_milestones = db[coll_name_milestones]
 coll_name_issues = db[coll_name_issues]
 
-
-#tags
-#repo
-repo_url_tag = 'Repo_URL'
-repo_id_tag = 'Repo_ID'
-
-#dev
-dev_name_tag = 'Dev_NAME'
-dev_id_tag = 'Dev_ID'
-
-#general
-url_tag = 'url'
-actor_tag = 'actor'
-login_tag = 'login'
-
-#repos urls
-repo_names = ["ankitkumar93/csc510-se-project", "jordy-jose/CSC_510_group_d", "moharnab123saikia/CSC510-group-f", "cleebp/csc-510-group-g"]
-
-#url prefix
-gitapi_url_prefix = "https://api.github.com/repos/"
-github_url_prefix = "https://github.com/"
-
-#ignores
-dev_ignore_list = ["timm", "history"]
 
 milestones_cursor = coll_name_milestone_raw.find()
 
@@ -57,27 +34,28 @@ def fetch_issues(milestone_id):
     return dev_list
 
 
-#filter milestone
+# filter milestone
 def filter_milestones():
     print("Filterng milestones Init")
-    id = 0
     for milestone in milestones_cursor:
         Description = milestone['description']
         Created_Timestamp = milestone['created_at']
         Due_Timestamp = milestone['due_on']
         Closed_Timestamp = milestone['closed_at']
         milestone_id = milestone['id']
-        fetch_issues(milestone_id)
-        insert_milestone(Created_Timestamp,Due_Timestamp,Closed_Timestamp,milestone_id)
+        milestone_title = milestone['title']
+        #fetch_issues(milestone_id)
+        insert_milestone(Created_Timestamp,Due_Timestamp,Closed_Timestamp,milestone_id,milestone_title)
         
 
-    print("Anonymize Devs Done")
+    print("Filterng milestones Done")
 
-# # insert milestone
-def insert_milestone(Created_Timestamp, Due_Timestamp, Closed_Timestamp,milestone_id):
-  print('inside insert ')
+# insert milestone
+def insert_milestone(Created_Timestamp, Due_Timestamp, Closed_Timestamp,milestone_id,milestone_title):
+  #print('inside insert ')
   coll_name_milestones.insert_one({
     "Milestone_ID" : milestone_id,
+    "Milestone_Title" : milestone_title,
     "Created_Timestamp" : Created_Timestamp,
     "Due_Timestamp" : Due_Timestamp,
     "Closed_Timestamp" : Closed_Timestamp
